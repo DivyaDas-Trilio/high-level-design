c711b4f07c8f18b28eb33e382ea6016bac05cd0d
Divya Das <divyajyotidas15@gmail.com>
2025-08-20 17:21:39 +0530

    Redis String DataType.

--- caches/redis/python-impl/config.py
+++ caches/redis/python-impl/config.py
@@ -1,10 +1,7 @@
 
 from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=['settings.yaml', '.secrets.yaml'],
-)
+settings = Dynaconf(envvar_prefix="DYNACONF", settings_files=['settings.yaml', '.secrets.yaml'])
+import pdb; pdb.set_trace()
 settings.setenv('default')
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
--- caches/redis/python-impl/redis_client.py
+++ caches/redis/python-impl/redis_client.py
@@ -1,9 +1,21 @@
 import redis
-from config import settings
+from requests import session
+#from config import settings
 
-redis_conf = settings.dev.redis
-conn = redis.Redis(host=redis_conf.REDIS_HOST, port=redis_conf.REDIS_PORT)
+#redis_conf = settings.default.redis
 
-# set key and get key.
-conn.set("name", "Divya")
-print(conn.get(name="name"))
+def create_connection():
+    conn = redis.Redis(host="172.26.0.56", port=30079)
+    return conn
+
+if __name__ == '__main__':
+    conn = create_connection() 
+    # set key and get key.
+    # conn.set("session", "SeSsIoN", nx=True, ex=20)
+    # conn.set("session", "SeSsIo", ex=20)
+    
+    # print(conn.get(name="session"))
+    
+    # List operation on.
+    conn.lpush('session', 10,20)
+    print(conn.lrange(name=session, start=0, end=-1))
